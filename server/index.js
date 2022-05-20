b901a6b1e7ab500ccc614c7aa6930dca0bc85a16
DhrumilShah98 <shah.dhrumil1998@gmail.com>
2022-05-20 13:42:32 -0300

    Server setup with Express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
