71a9192057f586e5245a001b133ebeba7bb578f3
DhrumilShah98 <shah.dhrumil1998@gmail.com>
2022-05-21 08:48:20 -0300

    Server: Added helmet for express app security

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import dotenv from "dotenv";
-
 import peopleRoutes from "./routes/people.js";
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 
-app.use("/people", peopleRoutes);
 app.use(cors());
-
-const PORT = process.env.PORT || 5000;
+app.use(helmet());
+app.use("/people", peopleRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
